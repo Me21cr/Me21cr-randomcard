afd18818dca5c639bfb82ba61fc3600d2ee3b3f9
Me21cr <128864515+Me21cr@users.noreply.github.com>
2023-07-27 18:45:22 +0000

    nuevo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  let simbolo = [
+    { S: "♦", C: "red" },
+    { S: "♥", C: "red" },
+    { S: "♠", C: "black" },
+    { S: "♣", C: "black" }
+  ];
+
+  let numero1 = numero[Math.floor(Math.random() * numero.length)];
+  let simbolo1 = simbolo[Math.floor(Math.random() * simbolo.length)];
+
+  document.querySelector(".letra").innerHTML = numero1;
+
+  document.querySelectorAll(".icono").forEach((elem, index) => {
+    elem.innerHTML = simbolo1.S;
+    elem.style.color = simbolo1.C;
+  });
 };
